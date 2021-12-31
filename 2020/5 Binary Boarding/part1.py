8da3af40bce18b1d0cc1fe5ef9127fe9feb95ca7
Benjamin <benjamin@lucidpress.com>
2021-12-31 14:23:02 -0700

    refactored solution for 2020 day 5 part 1

--- 2020/5 Binary Boarding/part1.py
+++ 2020/5 Binary Boarding/part1.py
@@ -1,31 +1,24 @@
 MAX_ROW = 127
 MAX_COL = 7
 
-def getRow(boardingPass):
-    newRange = (0,MAX_ROW)
-    for i in range(0,7):
-        if boardingPass[i] == 'F':
+def getPosition(boardingPass, maximum, upperLetter, lowerLetter, rangeToCheck):
+    newRange = (0,maximum)
+    for i in rangeToCheck:
+        if boardingPass[i] == upperLetter:
             newRange = (newRange[0], ((newRange[1]+newRange[0]))//2)
-        elif boardingPass[i] == 'B':
+        elif boardingPass[i] == lowerLetter:
             newRange = (((newRange[1]+newRange[0])+1)//2, newRange[1])
         else:
             raise Exception('Unexpect letter in boardingpass')
     if newRange[0] != newRange[1]:
-        raise Exception('you done fucked up getting the row')
+        raise Exception('unexpected item in bagging area')
     return newRange[0]
 
+def getRow(boardingPass):
+    return getPosition(boardingPass, MAX_ROW, 'F', 'B', range(0,7))
+
 def getCol(boardingPass):
-    newRange = (0,MAX_COL)
-    for i in range(7,10):
-        if boardingPass[i] == 'L':
-            newRange = (newRange[0], (newRange[1]+newRange[0])//2)
-        elif boardingPass[i] == 'R':
-            newRange = (((newRange[1]+newRange[0])+1)//2, newRange[1])
-        else:
-            raise Exception('Unexpect letter in boardingpass')
-    if newRange[0] != newRange[1]:
-        raise Exception('you done fucked up getting the column')
-    return newRange[0]
+    return getPosition(boardingPass, MAX_COL, 'L', 'R', range(7,10))
 
 def getSeatId(boardingPass):
     row = getRow(boardingPass)
