4998a65a88158165d27616d67ad043070a4fbd0f
Benjamin <benjamin@lucidpress.com>
2022-01-01 12:35:48 -0700

    solved 2020 day 6 part 2

--- 2020/6 Custom Customs/part2.py
+++ 2020/6 Custom Customs/part2.py
@@ -1,50 +1,29 @@
-from itertools import product
-MAX_ROW = 127
-MAX_COL = 7
+import string
 
-def getPosition(boardingPass, maximum, upperLetter, lowerLetter, rangeToCheck):
-    newRange = (0,maximum)
-    for i in rangeToCheck:
-        if boardingPass[i] == upperLetter:
-            newRange = (newRange[0], ((newRange[1]+newRange[0]))//2)
-        elif boardingPass[i] == lowerLetter:
-            newRange = (((newRange[1]+newRange[0])+1)//2, newRange[1])
-        else:
-            raise Exception('Unexpect letter in boardingpass')
-    if newRange[0] != newRange[1]:
-        raise Exception('unexpected item in bagging area')
-    return newRange[0]
+class Group:
+    def __init__(self, answers):
+        self.answers = answers
 
-def getRow(boardingPass):
-    return getPosition(boardingPass, MAX_ROW, 'F', 'B', range(0,7))
-
-def getCol(boardingPass):
-    return getPosition(boardingPass, MAX_COL, 'L', 'R', range(7,10))
-
-def getRowAndCol(boardingPass):
-    return (getRow(boardingPass), getCol(boardingPass))
-
-def hasFrontAndBackSeat(allSeats, seat):
-    return (seat[0]+1, seat[1]) in allSeats and (seat[0]-1, seat[1]) in allSeats
+    def getNumAnswers(self):
+        return len(self.answers)
 
 def main(input):
     with open(input) as f:
         lines = f.readlines()
 
-    allSeats = set()
+    soFar = set(list(string.ascii_lowercase))
+    groups = []
     for line in lines:
-        position = getRowAndCol(line)
-        allSeats.add(position)
+        if line.isspace():
+            groups.append(Group(soFar))
+            soFar = set(list(string.ascii_lowercase))
+        else:
+            person = set()
+            for c in line.strip():
+                person.add(c)
+            soFar = soFar.intersection(person)
+        
+    groups.append(Group(soFar))
 
-    allPossibleSeats = set(product(range(MAX_ROW+1), range(MAX_COL+1)))
-    missingSeats = allPossibleSeats.difference(allSeats)
-    mySeats = set()
-    for missingSeat in missingSeats:
-        if hasFrontAndBackSeat(allSeats, missingSeat):
-            mySeats.add(missingSeat)
-    if len(mySeats) != 1:
-        print('oops')
-    else:
-        mySeat = mySeats.pop()
-        print(mySeat[0]*8+mySeat[1])
-    
+    answer = sum(group.getNumAnswers() for group in groups)
+    print(answer)
