af2d2e0f78eaebea682bdd3ce45f8e0a73e1e0a0
Benjamin <benjamin@lucidpress.com>
2022-01-01 13:43:59 -0700

    solved 2020 day 7 part 2

--- 2020/7 Handy Haversacks/part2.py
+++ 2020/7 Handy Haversacks/part2.py
@@ -1,29 +1,35 @@
-import string
+def parseRule(line):
+    a = line.split('contain')
+    outterBag = a[0].replace('bags','').strip()
+    innerBags = []
+    if 'no other bags.' in a[1]:
+        return (outterBag, [])
+    bagsRaw = [i.strip() for i in a[1].split(',')]
+    for bagRaw in bagsRaw[:-1]:
+        count = int(bagRaw[0])
+        innerBags.append((count,bagRaw[1:].replace('bag' if count == 1 else 'bags', '').strip()))
+    count = int(bagsRaw[-1][0])
+    innerBags.append((count, bagsRaw[-1][1:-1].replace('bag' if count == 1 else 'bags', '').strip()))
 
-class Group:
-    def __init__(self, answers):
-        self.answers = answers
-
-    def getNumAnswers(self):
-        return len(self.answers)
+    return (outterBag, innerBags)
 
 def main(input):
     with open(input) as f:
         lines = f.readlines()
 
-    soFar = set(list(string.ascii_lowercase))
-    groups = []
+    bags = {}
     for line in lines:
-        if line.isspace():
-            groups.append(Group(soFar))
-            soFar = set(list(string.ascii_lowercase))
+        outterBag, innerBags = parseRule(line)        
+        if outterBag in bags:
+            bags[outterBag] += innerBags
         else:
-            person = set()
-            for c in line.strip():
-                person.add(c)
-            soFar = soFar.intersection(person)
-        
-    groups.append(Group(soFar))
+            bags[outterBag] = innerBags
 
-    answer = sum(group.getNumAnswers() for group in groups)
-    print(answer)
+    bagsToCheck = [(1,'shiny gold')]
+    numBags = -1
+    while len(bagsToCheck) != 0:
+        currBagAndCount = bagsToCheck.pop()
+        if currBagAndCount[1] in bags:
+            numBags += currBagAndCount[0]
+            bagsToCheck += [(i[0]*currBagAndCount[0], i[1]) for i in bags[currBagAndCount[1]]]
+    print(numBags)
