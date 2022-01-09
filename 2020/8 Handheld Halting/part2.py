c409ff89633d06f73829f38d6c6befb4e302ed0b
Benjamin <benjamin@lucidpress.com>
2022-01-09 13:40:52 -0700

    solved 2020 day 8 part 2

--- 2020/8 Handheld Halting/part2.py
+++ 2020/8 Handheld Halting/part2.py
@@ -1,35 +1,46 @@
-def parseRule(line):
-    a = line.split('contain')
-    outterBag = a[0].replace('bags','').strip()
-    innerBags = []
-    if 'no other bags.' in a[1]:
-        return (outterBag, [])
-    bagsRaw = [i.strip() for i in a[1].split(',')]
-    for bagRaw in bagsRaw[:-1]:
-        count = int(bagRaw[0])
-        innerBags.append((count,bagRaw[1:].replace('bag' if count == 1 else 'bags', '').strip()))
-    count = int(bagsRaw[-1][0])
-    innerBags.append((count, bagsRaw[-1][1:-1].replace('bag' if count == 1 else 'bags', '').strip()))
+from copy import deepcopy
 
-    return (outterBag, innerBags)
+def getInstructionValue(line):
+    return int(line[3:])
+
+def getAllVariations(lines):
+    variations = []
+    for i, line in enumerate(lines):
+        if line[:3] == 'nop':
+            variation = deepcopy(lines)
+            variation[i] = line.replace('nop', 'jmp')
+            variations.append(variation)
+        elif line[:3] == 'jmp':
+            variation = deepcopy(lines)
+            variation[i] = line.replace('jmp', 'nop')
+            variations.append(variation)
+    return variations
 
 def main(input):
     with open(input) as f:
         lines = f.readlines()
 
-    bags = {}
-    for line in lines:
-        outterBag, innerBags = parseRule(line)        
-        if outterBag in bags:
-            bags[outterBag] += innerBags
-        else:
-            bags[outterBag] = innerBags
-
-    bagsToCheck = [(1,'shiny gold')]
-    numBags = -1
-    while len(bagsToCheck) != 0:
-        currBagAndCount = bagsToCheck.pop()
-        if currBagAndCount[1] in bags:
-            numBags += currBagAndCount[0]
-            bagsToCheck += [(i[0]*currBagAndCount[0], i[1]) for i in bags[currBagAndCount[1]]]
-    print(numBags)
+    variations = getAllVariations(lines)
+    for variation in variations:
+        visited = set()
+        currLine = 0
+        accumulator = 0
+        foundError = False
+        while not currLine in visited:
+            if currLine == len(variation):
+                foundError = True
+                break
+            visited.add(currLine)
+            if variation[currLine][:3] == 'nop':
+                currLine += 1
+            elif variation[currLine][:3] == 'acc':
+                accumulator += getInstructionValue(variation[currLine])
+                currLine += 1
+            elif variation[currLine][:3] == 'jmp':
+                currLine += getInstructionValue(variation[currLine])
+            else:
+                raise Exception('Unexpected item in bagging area')
+        if foundError:
+            print(accumulator)
+            break
+    
